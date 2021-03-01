8a441c50a2ff0c1debf202c421499e1f7b5374e5
Luca-Willems <70705181+Luca-Willems@users.noreply.github.com>
2021-03-01 09:59:06 +0100

    korter geschreven

--- JumpForJoy.js
+++ JumpForJoy.js
@@ -102,12 +102,14 @@ function calcJump() {
         if (jumpingTime == 13) { //als hij de grond raakt is hij niet meer aan het springen
             isJumping = false
         }
-        else if (obstacleDelay == 1 && jumpingTime >= 4 && jumpingTime <= 7) { } //hier blijft hij hangen in de lucht
-        else if (obstacleDelay == 1 && jumpingTime <= 10) { //hij gaat naar beneden
-            r++
-        }
-        if (obstacleDelay == 1 && jumpingTime == 10) { //als hij de grond raakt is hij niet meer aan het springen
-            isJumping = false
+        if (obstacleDelay == 2) {
+            else if (jumpingTime >= 4 && jumpingTime <= 7) { } //hier blijft hij hangen in de lucht
+            else if (jumpingTime <= 10) { //hij gaat naar beneden
+                r++
+            }
+            if (jumpingTime == 10) { //als hij de grond raakt is hij niet meer aan het springen
+                isJumping = false
+            }
         }
     }
 }
